4d0c247ef9e506ac06a1a2424620debe746365d1
Rafael Peixoto <peixotterafael@gmail.com>
2017-05-13 11:59:20 +0200

    Loading Activity

---
+++ app/src/main/java/com/example/audreycelia/homeworkapp/LoadingActivity.java
@@ -0,0 +1,21 @@
+package com.example.audreycelia.homeworkapp;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_loading);
+
+    }
+
+
+}
